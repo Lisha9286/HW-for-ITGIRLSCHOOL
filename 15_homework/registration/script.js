483243355fd6f8e852155e15376694ada303468d
Lisha9286 <lisa@zverev.family>
2022-05-15 21:36:59 +0300

    add 15hw

--- 15_homework/registration/script.js
+++ 15_homework/registration/script.js
@@ -1,26 +1,27 @@
-function check(){
-    let resultName = document.getElementById('nameID').value;
-    if(resultName==''){
-        alert ("Enter Username!");
-    }
-
-    let resultEmail = document.getElementById('emailID').value;
-    if(resultEmail==''){
-        alert ("Enter email!");
-    }
-
-    let resultPhone = document.getElementById('phoneID').value;
-    if(resultPhone==''){
-        alert ("Enter phone number!");
-    }
-
-    let resultPassword = document.getElementById('passwordID').value;
-    if(resultPassword==''){
-        alert ("Create password!");
-    }
-
-    let resultConfirm = document.getElementById('confirmID').value;
-    if(resultConfirm==''){
-        alert ("Repeat password!");
+function check() {
+    let resultName = document.getElementById('nameID');
+    let resultConfirm = document.getElementById('confirmID');
+    let resultEmail = document.getElementById('emailID');
+    let resultPhone = document.getElementById('phoneID');
+    let resultPassword = document.getElementById('passwordID');
+    document.getElementById('errorMessege').innerHTML = "";
+    if (resultName.value == '') {
+        document.getElementById('errorMessege').innerHTML += "Enter Username!";
+    } else if (resultEmail.value == '') {
+        document.getElementById('errorMessege').innerHTML += "Enter email!";
+    } else if (resultEmail.value == '/^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i') {
+        //document.getElementById('errorMessege').innerHTML += "Enter correct email!";
+    } else if (resultPhone.value == '') {
+        document.getElementById('errorMessege').innerHTML += "Enter phone number!";
+    } else if (resultPhone.value.length <= 11) {
+        document.getElementById('errorMessege').innerHTML += "Enter correct phone number!";
+    } else if (resultPassword.value == '') {
+        document.getElementById('errorMessege').innerHTML += "Create password!";
+    } else if (resultPassword.value.length <= 7) {
+        document.getElementById('errorMessege').innerHTML += "Use 8 characters or more for your password";
+    } else if (resultConfirm.value == '') {
+        document.getElementById('errorMessege').innerHTML += "Repeat password!";
+    } else if (resultConfirm.value != resultPassword.value) {
+        document.getElementById('errorMessege').innerHTML += "Those passwords didn t match. Try again.";
     }
 }
---
+++ 15_homework/themes/script.js
@@ -0,0 +1,11 @@
+darkTheme.onclick = () => {    
+    document.body.classList.toggle("black")
+}
+
+lightTheme.onclick = () => {
+    document.body.classList.toggle("white")
+}
+
+rainbowTheme.onclick = () => {    
+    document.body.classList.toggle("rainbow")
+}
