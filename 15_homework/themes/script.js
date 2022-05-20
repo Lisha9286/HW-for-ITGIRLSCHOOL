79de5c5328a9a74a952a542bc9eef99989927f2d
Lisha9286 <lisa@zverev.family>
2022-05-20 21:38:44 +0300

    delete

--- 15_homework/themes/script.js
+++ 15_homework/themes/script.js
@@ -1,6 +1,5 @@
 
 function changeTheme(selectObject) {   
-    let clazz 
     let oldTheme = document.querySelector('body');
     oldTheme.classList.remove("white");
     oldTheme.classList.remove("black");
@@ -9,15 +8,12 @@ function changeTheme(selectObject) {
     switch (theme) {
         case 'rainbowTheme':
             document.body.classList.toggle("rainbow");
-            clazz = "rainbow";
             break;
         case 'blackTheme':
             document.body.classList.toggle("black");
-            clazz = "black";
             break;
         case 'whiteTheme':
             document.body.classList.toggle("white");
-            clazz = "white";
             break;
     }
 }
